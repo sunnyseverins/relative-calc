ccc6f17e425259bf73ad0de5a81e7e459e8a2d29
Alex Severins <severins.official@gmail.com>
2025-06-25 09:29:35 +0300

    fix: error when any Y = 0; dRemainder < 0 shows decimal instead of fraction; remove old code; improve output indentation

--- script.js
+++ script.js
@@ -4,38 +4,27 @@ function calc() {
     const y_max = +document.getElementById("y-max").value;
     const y_min = +document.getElementById("y-min").value;
 
-    if (!(w_max && w_min && y_max && y_min)) return document.getElementById("result").innerText = "Invalid data";
-    if (w_max <= w_min)                      return document.getElementById("result").innerText = "W max must be bigger than W min";
+    if (!(isFinite(w_max) && isFinite(w_min) && isFinite(y_max) && isFinite(y_min)))
+        return document.getElementById("result").innerText = "Invalid data";
+    if (w_max <= w_min) 
+        return document.getElementById("result").innerText = "W max must be bigger than W min";
 
     let result = '';
-    result = shortFormula_max(result);
-    result = longFormula_max(result);
-    // result = shortFormula_min(result);
-    // result = longFormula_min(result);
+    shortFormula();
+    longFormula();
+    document.getElementById("result").innerText = result;
 
-    function shortFormula_max(log) {
+    function shortFormula() {
         const k = (y_max - y_min) / (w_max - w_min);
         if (!isFinite(k) || k === 0) return document.getElementById("result").innerText = "Invalid data";
         const a = 100 * k;
         const b = w_max * k - y_max;
-        log += `\n Results:`
-        if (b === 0) return log += `\n${a}vw`;
-        log += `\n ${a.toFixed(2)}vw ${(b < 0) ? "+" : "-"} ${Math.abs(b.toFixed(2))}px`;
-        log += `\n${Math.abs(a.toFixed(4))} & ${Math.abs(b.toFixed(4))} for precision`;
-        return log;
+        result += `\n Results:`
+        if (b === 0) return result += `\ncalc(${a}vw)`;
+        result += `\n calc(${a.toFixed(2)}vw ${(b < 0) ? "+" : "-"} ${Math.abs(b.toFixed(2))}px)`;
+        result += `\n \xA0or \xA0${a.toFixed(4)} & ${Math.abs(b.toFixed(4))}`;
     }
-    // function shortFormula_min(log) {
-    //     const k = (y_max - y_min) / (w_max - w_min);
-    //     if (!isFinite(k) || k === 0) return document.getElementById("result").innerText = "Invalid data";
-    //     const a = 100 * k;
-    //     const b = w_min * k - y_min;
-    //     log += `\n\n By Min:`
-    //     if (b === 0) return log += `\n${a}vw`;
-    //     log += `\n ${a.toFixed(2)}vw ${(b < 0) ? "=" : "-"} ${Math.abs(b.toFixed(2))}px`;
-    //     log += `\n (${Math.abs(a.toFixed(4))} and ${Math.abs(b.toFixed(4))})`;
-    //     return log;
-    // }
-    function longFormula_max(log) {
+    function longFormula() {
         let c = (y_max - y_min) * 100;
         let d = (y_max - y_min) * w_max;
         let e = w_max - w_min;
@@ -65,45 +54,7 @@ function calc() {
         
         let dRemainder = d % (e / divisorForD);
         let dFull = Math.trunc(d / (e / divisorForD));
-        console.log(d, e / divisorForD);
     
-        log += `\nor\n ${c}vw / ${e / divisorForC} ${(dRemainder / e === 0) ? "" : (dRemainder < 0) ? `+ ${-dRemainder / (e / divisorForD)} ` : `- ${dRemainder} / ${(e / divisorForD)} `}${(y_max === dFull) ? "" : (y_max - dFull < 0) ? ` - ${dFull - y_max}` : ` + ${y_max - dFull}`}`;
-        return log;
+        result += `\n\xA0or\n ${c}vw / ${e / divisorForC} ${(dRemainder / e === 0) ? "" : (dRemainder < 0) ? `+ ${-dRemainder} / ${(e / divisorForD)} ` : `- ${dRemainder} / ${(e / divisorForD)} `}${(y_max === dFull) ? "" : (y_max - dFull < 0) ? ` - ${dFull - y_max}` : ` + ${y_max - dFull}`}`;
     }
-    // function longFormula_min(log) {
-    //     let c = (y_max - y_min) * 100;
-    //     let d = (y_max - y_min) * w_min;
-    //     let e = w_max - w_min;
-
-    //     let eCopy = e; 
-    //     let eDivisors = [];
-    //     for (let i = 2; i < Math.abs(eCopy);) {
-    //         if (eCopy % i === 0) {
-    //             eDivisors.push(i);
-    //             eCopy /= i;
-    //         } else i++;
-    //     }
-    //     let divisorForC = 1;
-    //     for (let j = eDivisors.length - 1; j >= 0; j--) {
-    //         if (c % eDivisors[j] === 0) {
-    //             c /= eDivisors[j];
-    //             divisorForC *= eDivisors[j];
-    //         }
-    //     }
-    //     let divisorForD = 1;
-    //     for (let k = eDivisors.length - 1; k >= 0; k--) {
-    //         if (d % eDivisors[k] === 0) {
-    //             d /= eDivisors[k];
-    //             divisorForD *= eDivisors[k];
-    //         }
-    //     }
-        
-    //     let dRemainder = d % (e / divisorForD);
-    //     let dFull = Math.trunc(d / (e / divisorForD));
-    //     console.log(d, e / divisorForD);
-    
-    //     log += `\nor\n ${c}vw / ${e / divisorForC} ${(dRemainder / e === 0) ? "" : (dRemainder < 0) ? `+ ${-dRemainder / (e / divisorForD)} ` : `- ${dRemainder} / ${(e / divisorForD)} `}${(y_min === dFull) ? "" : (y_min - dFull < 0) ? ` - ${dFull - y_min}` : ` + ${y_min - dFull}`}`;
-    //     return log;
-    // }
-    document.getElementById("result").innerText = result;
 }
